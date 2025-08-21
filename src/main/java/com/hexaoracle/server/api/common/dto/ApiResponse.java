d5d2dd61b84565b645d646e10471e4870417710f
sangja21 <ta7538@naver.com>
2025-08-22 08:55:26 +0900

    [CHORE] 테스트 환경 및 기본 아키텍처 구조 세팅

---
+++ src/main/java/com/hexaoracle/server/api/common/dto/ApiResponse.java
@@ -0,0 +1,32 @@
+package com.hexaoracle.server.api.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse<T> {
+    private int status;
+    private String message;
+    private T data;
+
+    public static <T> ApiResponse<T> success(T data) {
+        return ApiResponse.<T>builder()
+                .status(201)
+                .message("success")
+                .data(data)
+                .build();
+    }
+
+    public static <T> ApiResponse<T> error(int status, String message) {
+        return ApiResponse.<T>builder()
+                .status(status)
+                .message(message)
+                .data(null)
+                .build();
+    }
+}
